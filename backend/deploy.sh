d6c3e8655af838d045ba92eeaeba9a65b89752d4
Shirkunov Artyom <sshiae@localhost.localdomain>
2023-07-02 23:11:15 +0300

    Добавил DOCKERFILE в backend. Еще больше автоматизации

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Чтение пути от пользователя
+echo "Введите путь к исходным файлам backend:"
+read path
+
+# Копируем исходные файлы
+sudo cp -r $path docker-build-context
+sudo cp Dockerfile docker-build-context
+
+# Собираем образ
+sudo docker build -t northgatebackend-backend:latest docker-build-context/
+
+# Удаляем директорию
+sudo rm -rf docker-build-context
+
 # Читаем все секреты Docker
 source ../secrets.sh
 
