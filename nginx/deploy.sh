a0363cdadbe28de885abdd49d99c61b6cac819bd
Пшеничникова Ольга Максимовна (psheom) <psheom@maxi-net.ru>
2023-07-17 18:01:55 +0300

    Работа над Nginx-ом
    * Зафиксировали версию nginx: 1.25.1
    * Добавили в репозиторий файлы:
      * options-ssl-nginx.conf
      * ssl-dhparams.pem
    * Актуализировали default.conf
    * Временно ушли от certbot-а

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-# Создаём ворох папок для хранения данных
-sudo mkdir -p /home/docker_volumes/certbot
-sudo mkdir -p /home/docker_volumes/certbot/conf
-sudo mkdir -p /home/docker_volumes/certbot/www
-sudo mkdir -p /home/docker_volumes/certbot/conf/live/
-sudo mkdir -p /home/docker_volumes/certbot/conf/live/northgatevologda.ru/
-sudo mkdir -p /home/docker_volumes/certbot/conf/live/api.northgatevologda.ru/
-sudo mkdir -p /home/docker_volumes/certbot/conf/live/db.northgatevologda.ru/
-sudo mkdir -p /home/docker_volumes/certbot/conf/live/pgadmin.northgatevologda.ru/
-sudo mkdir -p /home/docker_volumes/certbot/conf/live/nifi.northgatevologda.ru/
-sudo mkdir -p /home/docker_volumes/static
-sudo mkdir -p /home/docker_volumes/media
+# Скрипт необходимо запустить через sudo
 
 # Создём сеть, если она не существует
-sudo docker network create --driver overlay --attachable northgatevologda
+docker network create --driver overlay --attachable northgatevologda
 
 # Удаляем стек, если он существует
-sudo docker stack rm nginxstack
+docker stack rm nginx
 
-# Строим образ и создаём docker стек
-sudo docker build -t nginx:latest .
-sudo docker stack deploy --compose-file docker-compose.yml nginxstack
+# Строим образ и деплоим стек
+docker build -t ng_nginx:latest .
+docker stack deploy --compose-file docker-compose.yml nginx
+
+# В будущем нужно будет решить будем ли мы копировать конфигурацию или
+# интегрировать её в образ
+#docker cp defualt.conf \
+#    $(docker ps --filter "name=nginx_nginx" -q):/etc/nginx/conf.d/
