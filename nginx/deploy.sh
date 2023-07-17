f42289b0dd41c3950b033c925e8d4285a957ccc0
Пшеничникова Ольга Максимовна (psheom) <psheom@maxi-net.ru>
2023-07-17 14:56:47 +0300

    Правки
    * Правки в скрипта
    * Правки в документации
    * Начинаем реанимировать Nifi

--- nginx/deploy.sh
+++ nginx/deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Создаём ворох папок для хранения данных
-sudo mkdir /home/docker_volumes/certbot
-sudo mkdir /home/docker_volumes/certbot/conf
-sudo mkdir /home/docker_volumes/certbot/www
-sudo mkdir /home/docker_volumes/certbot/conf/live/
-sudo mkdir /home/docker_volumes/certbot/conf/live/northgatevologda.ru/ # adjust to your domain
-sudo mkdir /home/docker_volumes/certbot/conf/live/api.northgatevologda.ru/
-sudo mkdir /home/docker_volumes/certbot/conf/live/db.northgatevologda.ru/
-sudo mkdir /home/docker_volumes/certbot/conf/live/pgadmin.northgatevologda.ru/
-sudo mkdir /home/docker_volumes/certbot/conf/live/nifi.northgatevologda.ru/
-sudo mkdir /home/docker_volumes/static
-sudo mkdir /home/docker_volumes/media
+sudo mkdir -p /home/docker_volumes/certbot
+sudo mkdir -p /home/docker_volumes/certbot/conf
+sudo mkdir -p /home/docker_volumes/certbot/www
+sudo mkdir -p /home/docker_volumes/certbot/conf/live/
+sudo mkdir -p /home/docker_volumes/certbot/conf/live/northgatevologda.ru/
+sudo mkdir -p /home/docker_volumes/certbot/conf/live/api.northgatevologda.ru/
+sudo mkdir -p /home/docker_volumes/certbot/conf/live/db.northgatevologda.ru/
+sudo mkdir -p /home/docker_volumes/certbot/conf/live/pgadmin.northgatevologda.ru/
+sudo mkdir -p /home/docker_volumes/certbot/conf/live/nifi.northgatevologda.ru/
+sudo mkdir -p /home/docker_volumes/static
+sudo mkdir -p /home/docker_volumes/media
 
 # Создём сеть, если она не существует
 sudo docker network create --driver overlay --attachable northgatevologda
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # Создём сеть, если она не существует
 sudo docker network create --driver overlay --attachable northgatevologda
 
-# Запускаем в определенном порядке все 
+# Запускаем в определенном порядке все
 sudo docker stack deploy --compose-file nginx/docker-compose.yml nginxstack
 sudo docker stack deploy --compose-file postgres/docker-compose.yml dbstack
 sudo docker stack deploy --compose-file backend/docker-compose.yml backendstack
