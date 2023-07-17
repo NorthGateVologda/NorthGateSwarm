e25437c00e1e3a38bb9068a3b7fa0320aceb57e3
Пшеничникова Ольга Максимовна (psheom) <psheom@maxi-net.ru>
2023-07-17 19:40:17 +0300

    Перенастраиваем Nifi

--- nifi/deploy.sh
+++ nifi/deploy.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# Создём сеть, если она не существует
-sudo docker network create --driver overlay --attachable northgatevologda
-
-# Удаляем стек
-sudo docker stack rm nifistack
-
-# Создаем необходимые директории
-sudo mkdir /home/docker_volumes/nifi
+# Скрипт необходимо запустить через sudo
 
 # Создаем стэк контейнеров
-sudo docker stack deploy --compose-file docker-compose.yml nifistack
+docker stack deploy --compose-file docker-compose.yml nifi
---
+++ nifi/undeploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Скрипт необходимо запустить через sudo
+
+docker stack rm nifi
